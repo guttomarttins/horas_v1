20560ee404c17bed8ab8a14d1ccc0590e2091d6e
guttomarttins <guttomarttins@gmail.com>
2025-02-22 08:57:54 -0300

    feat: Initial setup of Flutter project with Firebase integration
    This commit introduces the initial structure and configuration for a new Flutter application, including:
    
    -   **Firebase Integration**:
        -   Added `firebase_core`, `cloud_firestore`, `firebase_auth`, `firebase_messaging`, and `google_sign_in` dependencies in `pubspec.yaml`.
        -   Configured Firebase for Android and web platforms via `firebase.json`, `google-services.json`, and `firebase_options.dart`.
    -   **Android Configuration**:
        -   Created necessary Android project files (`build.gradle`, `AndroidManifest.xml`, `gradle.properties`, etc.).
        -   Set up application details and permissions in `AndroidManifest.xml`.
        -   Configured launch themes in `styles.xml`.
        -   Added icons for the android app.
        -   Added `MainActivity.java` file.
    -   **Web Configuration**:
        -   Added `index.html` and `manifest.json` for web app details.
        -   Added icons for web app.
    -   **Core Structure**:
        -   Created a basic `main.dart` file to initialize Firebase and the app.
        -   Created a basic `login_screen.dart` to be the landing page of the app.
    - **Analysis options**:
        - Created a `analysis_options.yaml` file to configure the analyzer.
    -   **Other**:
        -   Added `.gitignore` files for both root and Android directories.
        - Created a `README.md` file.
        - Created `test/widget_test.dart` file.
    -   Added `pubspec.lock` file.
    
    These changes establish the foundational environment for further development.

---
+++ android/app/src/main/java/br/com/dev/horas_v1/MainActivity.java
@@ -0,0 +1,6 @@
+package br.com.dev.horas_v1;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:horas_v1/screens/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:horas_v1/firebase_options.dart';
+
+Future<void> main() async{
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
